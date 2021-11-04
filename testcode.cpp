35939a2e0ba06bedfda4f81b4f6cc1b7af8ec207
Wolf-256 <93338953+Wolf-256@users.noreply.github.com>
2021-11-04 18:45:25 -0400

    Create testcode.cpp

---
+++ testcode.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int unit = 43560;
+float ImputAreaFeet=0;
+float OutputNumberOfAcres=0;
+
+cout << " WELCOME TO UNIT CONVERTER FEET TO ACRES " << endl;
+cout << " PLEASE ESTIMATE TO THE NEAREST WHOLE NUMBER " << endl;
+cout << " ENTER SIZE OF AREA IN FEET: ";
+
+OutputNumberOfAcres=ImputAreaFeet/unit;
+
+cout << "Number of Acres is about: "OutputNumberOfAcres;
+
+return 0;
+}
