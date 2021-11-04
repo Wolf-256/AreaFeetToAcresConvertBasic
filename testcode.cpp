3eea347b12b19c74b65202fc64a651b7d0c60579
Wolf-256 <93338953+Wolf-256@users.noreply.github.com>
2021-11-04 19:11:04 -0400

    v1.02
    v1.02

--- testcode.cpp
+++ testcode.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main(){
+//variables 
 int unit = 43560;
 float ImputAreaFeet;
 float OutputNumberOfAcres;
 
+//text for user
 cout << " WELCOME TO UNIT CONVERTER FEET TO ACRES " << endl;
 cout << " PLEASE ESTIMATE TO THE NEAREST WHOLE NUMBER " << endl;
 cout << " ENTER SIZE OF AREA IN FEET: ";
+
+//defines our number of sq ft units to calculate 
 cin >> ImputAreaFeet;
 
+//calculate
 OutputNumberOfAcres=ImputAreaFeet/unit;
 
+//cleans up output
+round(OutputNumberOfAcres);
+
+//output
 cout << "Number of Acres is about: " << OutputNumberOfAcres << " units.";
 
 return 0;
