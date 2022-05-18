8070fa60d1e881b6d2fa54bd64554d14e043c2f1
Wolf-256 <93338953+Wolf-256@users.noreply.github.com>
2022-05-18 15:19:18 -0400

    Update testcode.cpp
    reduce lines of code

--- testcode.cpp
+++ testcode.cpp
@@ -8,19 +8,14 @@ int main()
 const int unit = 43560;
 float ImputAreaFeet;
 float OutputNumberOfAcres;
-string welcomeTexts[3] = {"WELCOME TO UNIT CONVERTER FEET TO ACRES", "PLEASE ESTIMATE TO THE NEAREST WHOLE NUMBER", "ENTER SIZE OF AREA IN FEET:"};
-//text for user
+string welcomeTexts[3] = {"WELCOME TO UNIT CONVERTER FEET TO ACRES", "PLEASE ESTIMATE TO THE NEAREST WHOLE NUMBER", "ENTER SIZE OF AREA IN FEET:"};//text for user
 for(int j=0;j<3;j++)
 {
 cout  << welcomeTexts[j] << "\n";
 }
-//defines our number of sq ft units to calculate 
-cin >> ImputAreaFeet;
-//calculate
-OutputNumberOfAcres=ImputAreaFeet/unit;
-//cleans up output
-round(OutputNumberOfAcres);
-//output
-cout << "The area of acres is about: " << OutputNumberOfAcres << " unit(s) . ";
+cin >> ImputAreaFeet;//defines our number of sq ft units to calculate 
+OutputNumberOfAcres=ImputAreaFeet/unit;//calculate
+round(OutputNumberOfAcres);//cleans up output
+cout << "The area of acres is about: " << OutputNumberOfAcres << " unit(s) . ";//output
 return 0;
 }
